9df22556282a3a28fb153f52d47da563130659d5
Kirill Rasuli <rasulikv@gmail.com>
2020-08-02 22:51:54 +0300

    All functions are fixed to work correctly

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dvmn_vk_autoposter
+# Comics publisher
+
+This is the app automatically posting to a random XCKD comic to your VK group.
+
+### How to use
+
+You'll need to create two environment variables with your VK access token group ID where to post:
+```
+ACCESS_TOKEN='your access token'
+GROUP_ID='your group ID'
+```
+You can put them into .env file next to main.py.
+
+Just run the python script main.py with the following concole command:
+```
+python3 main.py
+```
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from pathlib import Path
-import pprint
 import json
 import os
 from dotenv import load_dotenv
+import random
+from pathlib import Path
 
 
 
@@ -14,27 +15,34 @@ def get_file_extension(url):
 def download_a_pic(url, filename):
     response = requests.get(url)
     response.raise_for_status()
-    pic_extension = get_file_extension(url)
 
-    with open(Path.cwd().joinpath(f'{filename}{pic_extension}'), 'wb') as image:
+    with open(Path.cwd().joinpath(filename), 'wb') as image:
         image.write(response.content)
+
 
 def get_a_comic(number_of_comic):
     url = f'http://xkcd.com/{number_of_comic}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
     comic_pic = response.json()
-    download_a_pic(comic_pic['img'], comic_pic['title'])
-    # print(comic_pic['alt'])
+    pic_extension = get_file_extension(comic_pic['img'])
+    pic_filename = f"{comic_pic['title']}{pic_extension}"
+    download_a_pic(comic_pic['img'], pic_filename)
+    return pic_filename, comic_pic['alt']
 
-def get_upload_url(token):
+
+def get_upload_url(token, group_id):
     vk_url = 'https://api.vk.com/method/photos.getWallUploadServer'
-    vk_params = {'access_token': token, 'v': '5.122', 'group_id': '197617370'}
+    vk_params = {'access_token': token, 'v': '5.122', 'group_id': group_id}
     response = requests.get(vk_url, params=vk_params)
+    response.raise_for_status()
     return response.json()['response']['upload_url']
 
-def post_a_comic(upload_url, token):
-    with open('Python.png', 'rb') as file:
+
+def post_a_comic(upload_url, token, message, pic_filename, group_id):
+    """put all three requests to VK api in one function cause they can be used only together"""
+    
+    with open(pic_filename, 'rb') as file:
         files = {'photo': file}
         response = requests.post(upload_url, files=files)
         response.raise_for_status()
@@ -42,24 +50,35 @@ def post_a_comic(upload_url, token):
 
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     params = {'server': uploaded_pic['server'], 'photo': uploaded_pic['photo'], 'hash': uploaded_pic['hash'],
-    'access_token': token, 'v': '5.122', 'group_id': '197617370'}
+    'access_token': token, 'v': '5.122', 'group_id': group_id}
     response = requests.post(url, params=params)
+    response.raise_for_status()
     saved_pic = response.json()
-    pprint.pprint(saved_pic)
-
+    
     url = 'https://api.vk.com/method/wall.post'
     params = {'attachments': f"photo{saved_pic['response'][0]['owner_id']}_{saved_pic['response'][0]['id']}",
-    'access_token': token, 'v': '5.122', 'group_id': '197617370'}
+    'access_token': token, 'v': '5.122', 'owner_id': f'-{group_id}', 'from_group': '1', 'message': message}
     response = requests.post(url, params=params)
-    pprint.pprint(response.json())
+    response.raise_for_status()
+
+
+def get_number_of_comics():
+    url = f'http://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    last_pic = response.json()
+    return random.randint(0, last_pic['num'])
+
 
 def main():
     load_dotenv()
     vk_token = os.getenv('ACCESS_TOKEN')
-    # get_a_comic(353)
-    upload_url = get_upload_url(vk_token)
-    # print(upload_url)
-    post_a_comic(upload_url, vk_token)
+    group_id = os.getenv('GROUP_ID')
+    number_of_comic = get_number_of_comics()
+    pic_filename, message = get_a_comic(number_of_comic)
+    upload_url = get_upload_url(vk_token, group_id)
+    post_a_comic(upload_url, vk_token, message, pic_filename, group_id)
+    Path.unlink(Path.cwd().joinpath(pic_filename))
 
 
 if __name__ == '__main__':
